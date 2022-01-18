d8ac969e4e9a11a53e161300d39b78b46df84607
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2022-01-18 14:02:40 +0100

    Initial commit, outputing only battery percentage.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module icebat
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type responseType struct {
+	StatusGetRSP struct {
+		WebHeader struct {
+			BatteryChargeLevelPercentage int
+		}
+	}
+}
+
+func main() {
+	url := "http://192.168.0.1/fcgi/cusconf.fcgi"
+	jsonString := []byte(`{"StatusGetReq":{"Oper":"WebHeader"}}`)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonString))
+	if err != nil {
+		log.Fatalf("Unable to create request: %s", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Got error performing request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %s", err)
+	}
+
+	res := responseType{}
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		log.Fatalf("Error unmarshaling response: %s", err)
+	}
+
+	fmt.Println(res.StatusGetRSP.WebHeader.BatteryChargeLevelPercentage)
+}
